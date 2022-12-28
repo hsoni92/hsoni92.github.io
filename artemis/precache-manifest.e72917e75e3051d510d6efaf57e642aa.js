9c5df8b7b49e6758f5e2b3144560b82a82a42757
himanshu.soni <himanshu.soni@nutanix.com>
2022-12-29 05:02:31 +0530

    Initial commit himanshu.soni-14-09-2023-18:06:26

---
+++ artemis/precache-manifest.e72917e75e3051d510d6efaf57e642aa.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ad4950744cd0836fba4",
+    "url": "/artemis/static/css/main.76592954.chunk.css"
+  },
+  {
+    "revision": "2ad4950744cd0836fba4",
+    "url": "/artemis/static/js/main.53de4c89.chunk.js"
+  },
+  {
+    "revision": "a2fda3a838c371c1f81b",
+    "url": "/artemis/static/js/runtime~main.46c87c3d.js"
+  },
+  {
+    "revision": "6b760009f8ca3ff525b2",
+    "url": "/artemis/static/css/2.b957cf7d.chunk.css"
+  },
+  {
+    "revision": "6b760009f8ca3ff525b2",
+    "url": "/artemis/static/js/2.740d1f3b.chunk.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/artemis/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/artemis/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/artemis/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/artemis/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/artemis/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/artemis/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/artemis/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/artemis/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/artemis/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/artemis/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/artemis/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/artemis/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/artemis/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/artemis/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/artemis/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/artemis/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c83051068a9c49f92a249893239c693f",
+    "url": "/artemis/static/media/logo.c8305106.svg"
+  },
+  {
+    "revision": "4b0098a98d1d393bd7fdf6b8b8fcaae0",
+    "url": "/artemis/index.html"
+  }
+];
---
+++ components/precache-manifest.f81b5f3866ae5a72d653f218b82ec97f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d9fcf3892ea380b2306",
+    "url": "/components/static/css/main.77ed89cf.chunk.css"
+  },
+  {
+    "revision": "3d9fcf3892ea380b2306",
+    "url": "/components/static/js/main.0875de06.chunk.js"
+  },
+  {
+    "revision": "0d00e6050ae09c463297",
+    "url": "/components/static/js/runtime~main.35b5100b.js"
+  },
+  {
+    "revision": "7afb658555628c57636f",
+    "url": "/components/static/js/2.62b629eb.chunk.js"
+  },
+  {
+    "revision": "6340b90800e5768a9ba605d1bb119388",
+    "url": "/components/static/media/visa.6340b908.svg"
+  },
+  {
+    "revision": "751793ea4d68de7115e7f02a82e8a31a",
+    "url": "/components/static/media/bank.751793ea.svg"
+  },
+  {
+    "revision": "64b4a5b4be7da606369d8524ffa2a9c5",
+    "url": "/components/index.html"
+  }
+];
