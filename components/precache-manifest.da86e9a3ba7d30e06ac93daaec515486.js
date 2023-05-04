ae5582fb491fe218bfc07961d0c502a08481bf60
himanshu.soni <himanshu.soni@nutanix.com>
2023-05-04 23:36:15 +0530

    himanshu-04-05-2023-23:36:15

---
+++ components/precache-manifest.da86e9a3ba7d30e06ac93daaec515486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1680bec6cc78b8575bd",
+    "url": "/static/css/main.77ed89cf.chunk.css"
+  },
+  {
+    "revision": "a1680bec6cc78b8575bd",
+    "url": "/static/js/main.23ff5f92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c07011fb2b4309c8d7ba",
+    "url": "/static/js/2.d8710211.chunk.js"
+  },
+  {
+    "revision": "6340b90800e5768a9ba605d1bb119388",
+    "url": "/static/media/visa.6340b908.svg"
+  },
+  {
+    "revision": "751793ea4d68de7115e7f02a82e8a31a",
+    "url": "/static/media/bank.751793ea.svg"
+  },
+  {
+    "revision": "aab10d53ea1f907d9c4495c8e1bfe6b3",
+    "url": "/index.html"
+  }
+];
