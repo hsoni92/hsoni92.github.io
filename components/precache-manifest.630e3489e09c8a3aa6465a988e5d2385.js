c43a039df35765871d56f9f1feb7996c4eeca6e3
himanshu.soni <himanshu.soni@nutanix.com>
2023-05-05 13:50:56 +0530

    himanshu-05-05-2023-13:50:56

---
+++ components/precache-manifest.630e3489e09c8a3aa6465a988e5d2385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "877350d2d42646b51737",
+    "url": "/components/static/css/main.77ed89cf.chunk.css"
+  },
+  {
+    "revision": "877350d2d42646b51737",
+    "url": "/components/static/js/main.066bbafc.chunk.js"
+  },
+  {
+    "revision": "0d00e6050ae09c463297",
+    "url": "/components/static/js/runtime~main.35b5100b.js"
+  },
+  {
+    "revision": "a5097d889c3474f4aa0f",
+    "url": "/components/static/js/2.7d3375cf.chunk.js"
+  },
+  {
+    "revision": "6340b90800e5768a9ba605d1bb119388",
+    "url": "/components/static/media/visa.6340b908.svg"
+  },
+  {
+    "revision": "751793ea4d68de7115e7f02a82e8a31a",
+    "url": "/components/static/media/bank.751793ea.svg"
+  },
+  {
+    "revision": "2ba348b064f464c63b19d7d897020b31",
+    "url": "/components/index.html"
+  }
+];
--- components/service-worker.js
+++ components/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4fbfe4472f278d4499b8534fedab594.js"
+  "/components/precache-manifest.630e3489e09c8a3aa6465a988e5d2385.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/components/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
